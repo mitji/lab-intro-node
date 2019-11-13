bb6a8bd452b5102f82f3c40e7fc6c32089f353ed
David Mitjana Castro <d.mitjana@gmail.com>
2019-11-13 14:09:29 +0100

    lab done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,58 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+    constructor() {
+      this.items = [];
+      this.length = 0;
+    }
+    add(item) {
+      this.items.push(item);
+      this.length++;
+    }
+    get(pos) {
+      if (pos>=this.length) {
+        throw new Error('OutOfBounds');
+      } else {
+        this.items.sort( (a,b) => {
+          return a-b;
+        })
+        return this.items[pos];
+      }
+    }
+    max() {
+      if(this.length === 0) {
+        throw new Error('EmptySortedList');
+      } else {
+        return Math.max(...this.items);
+      }
+    }
+    min() {
+      if(this.length === 0) {
+        throw new Error('EmptySortedList');
+      } else {
+        return Math.min(...this.items);
+      }
+    }
+    average() {
+      if(this.length === 0) {
+        throw new Error('EmptySortedList');
+      } else {
+        let sum = this.items.reduce ( (accumulator, currentValue) => {
+          return accumulator += currentValue;
+        });
+        return sum/this.items.length;
+      }
+    }
+    sum() {
+      let sum = 0;
+      if(this.length === 0) {
+        return sum;
+      } else {
+        sum = this.items.reduce ( (accumulator, currentValue) => {
+          return accumulator += currentValue;
+        });
+        return sum;
+      }
+    }
+};
+
+module.exports = SortedList;
+
